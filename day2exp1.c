9b24ea1faf5cc4886f12da5f0f3af8bd3b776edb
harsh-45coder <harsh.ashu45@gmail.com>
2026-02-07 22:22:05 +0530

    Update day2exp1.c

--- day2exp1.c
+++ day2exp1.c
@@ -10,26 +10,23 @@ int main() {
     int n, pos;
     int arr[100];
 
-    // Read number of elements
     scanf("%d", &n);
-
-    // Read array elements
+    
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    // Read position to delete (1-based)
     scanf("%d", &pos);
 
-    // Shift elements to the left
+   
     for (int i = pos - 1; i < n - 1; i++) {
         arr[i] = arr[i + 1];
     }
 
-    // Print updated array
     for (int i = 0; i < n - 1; i++) {
         printf("%d ", arr[i]);
     }
 
     return 0;
 }
+
