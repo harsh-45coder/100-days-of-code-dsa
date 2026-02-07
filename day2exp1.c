b616be7b91946d08eccf5b010b0ccc1aa97b54f7
harsh-45coder <harsh.ashu45@gmail.com>
2026-02-07 22:21:12 +0530

    Add files via upload

---
+++ day2exp1.c
@@ -0,0 +1,35 @@
+//Problem: Write a C program to delete the element at a given 1-based position pos from an array of n integers. Shift remaining elements to the left.
+
+//Input
+//- First line: integer n
+//- Second line: n space-separated integers
+//- Third line: integer pos (1-based position to delete)
+#include <stdio.h>
+
+int main() {
+    int n, pos;
+    int arr[100];
+
+    // Read number of elements
+    scanf("%d", &n);
+
+    // Read array elements
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Read position to delete (1-based)
+    scanf("%d", &pos);
+
+    // Shift elements to the left
+    for (int i = pos - 1; i < n - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+
+    // Print updated array
+    for (int i = 0; i < n - 1; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
