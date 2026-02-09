8be1123eb731218f8f6c4262a48d7232081b87fd
harsh-45coder <harsh.ashu45@gmail.com>
2026-02-09 22:28:28 +0530

    Add files via upload

---
+++ day9exp1.c
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int start = 0;
+    int end = s.length() - 1;
+
+    while (start < end) {
+        swap(s[start], s[end]);
+        start++;
+        end--;
+    }
+
+    cout << s;
+    return 0;
+}
