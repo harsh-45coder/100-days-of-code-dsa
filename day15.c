f18a4cdfe00d34880adbf2d4e24c7db44eb62134
harsh-45coder <harsh.ashu45@gmail.com>
2026-04-01 11:41:40 +0530

    Add files via upload

---
+++ day15.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    scanf("%d %d", &m, &n);
+
+    int matrix[100][100];
+
+    // Input matrix
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    int sum = 0;
+
+    // Sum of primary diagonal
+    for(int i = 0; i < m && i < n; i++) {
+        sum += matrix[i][i];
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
