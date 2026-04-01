0ea931a601053abb9dd734b229d1282220ecefc7
harsh-45coder <harsh.ashu45@gmail.com>
2026-04-01 11:40:22 +0530

    Add files via upload

---
+++ day14.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int matrix[100][100];
+
+    // Input matrix
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Check identity matrix
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            
+            if(i == j) {
+                if(matrix[i][j] != 1) {
+                    printf("Not an Identity Matrix");
+                    return 0;
+                }
+            } else {
+                if(matrix[i][j] != 0) {
+                    printf("Not an Identity Matrix");
+                    return 0;
+                }
+            }
+        }
+    }
+
+    printf("Identity Matrix");
+
+    return 0;
+}
